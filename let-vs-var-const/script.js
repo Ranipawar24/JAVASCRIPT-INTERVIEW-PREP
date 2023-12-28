96f3c1117bc981295183ce6782955fdeae784840
Rani Pawar <pawar.rani241996@gmail.com>
2023-12-28 10:26:31 +0530

    Update script.js

---
+++ let-vs-var-const/script.js
@@ -0,0 +1,20 @@
+// var vs let vs const
+// scope
+
+// function name() {
+//   // this is a function scope
+// }
+
+// {
+//   // this is will a block scope
+// }
+
+// var a = 5; // so var is a functional scope
+// console.log(a);
+
+{
+  // let and const is a block scope
+  let a = 6;
+  console.log(a);
+}
+// console.log(a);
