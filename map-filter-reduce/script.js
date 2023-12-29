5417da540e351c3fc9f12e1d4e62279a5e6bcfce
Rani Pawar <pawar.rani241996@gmail.com>
2023-12-29 13:28:17 +0530

    Update script.js

--- map-filter-reduce/script.js
+++ map-filter-reduce/script.js
@@ -28,18 +28,36 @@
 //Array.map((num,i,arr)=>{})
 
 // this is a costom map function
-Array.prototype.myMap = function (cb) {
+
+// Array.prototype.myMap = function (cb) {
+//   let temp = [];
+//   for (let i = 0; i < this.length; i++) {
+//     temp.push(cb(this[i], i, this));
+//   }
+//   return temp;
+// };
+
+// const nums = [1, 2, 3, 4];
+// const multiflyThree = nums.myMap((num, i, arr) => {
+//   return num * 3;
+// });
+// console.log(multiflyThree);
+
+//-------------------------//-------------------------------//
+//POLYFILL FOR FILTER()
+
+Array.prototype.myFilter = function (cb) {
   let temp = [];
   for (let i = 0; i < this.length; i++) {
-    temp.push(cb(this[i], i, this));
+    if (cb(this[i], i, this)) temp.push(this[i]);
   }
   return temp;
 };
-
-const nums = [1, 2, 3, 4];
-const multiflyThree = nums.myMap((num, i, arr) => {
-  return num * 3;
+const nums = [1, 2, 3, 4, 5, 6];
+const moreThanThree = nums.myFilter((num) => {
+  return num > 3;
 });
-console.log(multiflyThree);
+console.log(moreThanThree);
 
 //-------------------------//-------------------------------//
+//POLYFILL FOR REDUCE()
