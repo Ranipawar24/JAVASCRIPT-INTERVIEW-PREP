dface2698d75b3efa44409ff8b4ad442fbff13f5
Rani Pawar <pawar.rani241996@gmail.com>
2023-12-30 15:02:15 +0530

    Update script.js

--- Functions/script.js
+++ Functions/script.js
@@ -2,19 +2,31 @@
 
 // Q 1 - WHAT IS FUNCTION DECLARATION?
 
-function squre(num) {
-  // this is a function defination or declran
-  return num * num;
+// function squre(num) {
+//   // this is a function defination or declran
+//   return num * num;
+// }
+//-------------------------//-------------------------------//
+// // Q 2 - WHAT IS FUNCTION EXPRESSION?
+
+// // when we store a function inside a variable its called a function expression.
+
+// // example
+// const square = function (nums) {
+//   return nums * nums;
+// };
+// console.log(square(5));
+
+//-------------------------//-------------------------------//
+// Q 3 - WHAT IS FIRST CLass FUNCTION ?
+
+// we can paas them of another fun just like a variable and we can also manupulate retun them from of that fun is called first class fun.
+
+function square(nums) {
+  return nums * nums;
 }
 
-// Q 2 - WHAT IS FUNCTION EXPRESSION?
-
-// when we store a function inside a variable its called a function expression.
-
-// example
-const squree = function (nums) {
-  return nums * nums;
-};
-console.log(squree(5));
-
-// Q 2 - WHAT IS FUNCTION EXPRESSION?
+function displaySqure(fn) {
+  console.log("square is " + fn(5));
+}
+displaySqure(square);
