1887ed7b353a80bcd5dba089b74d0448e92244f5
Rani Pawar <pawar.rani241996@gmail.com>
2023-12-30 14:43:56 +0530

    Update script.js

---
+++ Functions/script.js
@@ -0,0 +1,20 @@
+// FINCTIONS IN JAVASCRIPT
+
+// Q 1 - WHAT IS FUNCTION DECLARATION?
+
+function squre(num) {
+  // this is a function defination or declran
+  return num * num;
+}
+
+// Q 2 - WHAT IS FUNCTION EXPRESSION?
+
+// when we store a function inside a variable its called a function expression.
+
+// example
+const squree = function (nums) {
+  return nums * nums;
+};
+console.log(squree(5));
+
+// Q 2 - WHAT IS FUNCTION EXPRESSION?
