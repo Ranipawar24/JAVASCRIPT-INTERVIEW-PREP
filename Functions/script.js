d0e8bc64754f0b22c01aa33b6009a86dab743034
Rani Pawar <pawar.rani241996@gmail.com>
2024-01-06 14:53:58 +0530

    Update script.js

--- Functions/script.js
+++ Functions/script.js
@@ -69,7 +69,17 @@
 // var a = 5;
 // // console.log(a);
 
-// Q 6 - FUNCTION HOISTING  ? OUTPUT BASED QUE ?
+// Q 7 - FUNCTION HOISTING  ? OUTPUT BASED QUE ?
+//when we have a variable present in a scope we will not go and check globel scope
+var x = 20;
+
+var fun = function () {
+  console.log(x);
+  var x = 21;
+};
+fun();
+
+// Q 8 - FUNCTION HOISTING  ? OUTPUT BASED QUE ?
 //when we have a variable present in a scope we will not go and check globel scope
 var x = 20;
 
