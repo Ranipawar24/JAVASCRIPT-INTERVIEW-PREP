755b7ae48de63de336f12e44506a4c5fcc0d75db
Rani Pawar <pawar.rani241996@gmail.com>
2024-01-20 15:42:30 +0530

    Update script.js

--- Functions/script.js
+++ Functions/script.js
@@ -115,15 +115,50 @@ function multyply(...nums) {
 var arr = [5, 6];
 multyply(...arr); // this is called spread operator
 
-// Q 10- PARAMS VS ARGUMENTS output based questions?
+// Q 12- PARAMS VS ARGUMENTS output based questions?
 // rest and spread parameters are must be the last formal parameters
 const fn = (a, x, y, ...numbers) => {
   console.log(x, y, numbers);
 };
 fn(5, 6, 3, 2, 8, 7);
 
-// Q 10- WHAT IS A CALLBACK FUNCTION?
+//-------------------------//-------------------------------//
+
+// Q 11- WHAT IS A CALLBACK FUNCTION?
 // A callback function is a function passed into another function as an argument, which is then invoked inside the outer function to
 //complete some kind of routine or action.
 
 document.addEventListener("click", function (params) {}); // when we passing a function inside another function an this fun is manupulated inside this owns
+
+// Q 13- WHAT IS A ARROW FUNCTION?
+
+const add = (firstName, secondName) => {
+  return firstName + secondName;
+};
+
+// Q 14- ARROW FUNCTION VS REGULAR FUNCTION?
+// 1.SYNTAX
+function squre(num) {
+  // regular
+  return num * num;
+}
+
+const scope = (num) => {
+  // arrow
+  return num * num;
+};
+
+//2.Implicit "return" keyword
+const scope1 = (num) => num * num;
+
+//3.arguments
+function fn2() {
+  console.log(arguments);
+}
+fn2(1, 2, 3);
+
+const fn1 = () => {
+  // arrow
+  console.log(arguments);
+};
+fn1(1, 2, 3);
